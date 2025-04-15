d13612cb32f80dae5270dc75eb0dd936577804fe
rramish <rohullahramish.work@gmail.com>
2025-04-15 18:42:18 +0500

    imp: block width changed

--- src/app/main/components/ImageSlicer.tsx
+++ src/app/main/components/ImageSlicer.tsx
@@ -50,7 +50,7 @@ const ImageSlicerWithDrawing = ({
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      setFixedWidth(window.innerWidth - 100);
+      setFixedWidth(window.innerWidth - 200);
     }
     const rows = 5;
     const cols = 4;
