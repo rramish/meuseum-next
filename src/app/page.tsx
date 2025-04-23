48b8a4ff3cb331d4571ef41c886c549d6455bdd3
rramish <rohullahramish.work@gmail.com>
2025-04-23 22:39:08 +0500

    fix: build error solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 const Hpmepage = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
+      // eslint-disable-next-line  @typescript-eslint/no-explicit-any
       window.location.href = "/home" as any;
     }
   }, []);
