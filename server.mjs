ad9e41e080f206907862197d5243fd95cd8ab176
rramish <rohullahramish.work@gmail.com>
2025-05-01 23:39:21 +0500

    final heroku issue resolved

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import { createServer } from "node:http";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
